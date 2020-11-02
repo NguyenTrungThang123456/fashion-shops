c16aa2fee5144f93759f62d55ea2078c8d8b393f
NguyenTrungThang123456 <thangnt.nde18074@vtc.edu.vn>
2020-11-02 14:11:50 +0700

    Project Setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    `mongodb+srv://thang:thang@testcluster1.bt7hu.mongodb.net/fashion?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+)
+
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+})
