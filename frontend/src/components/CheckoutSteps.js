b7293d8a4738b709bcc55a1cc237ad44d5b2b7e5
trungthang1610 <thangnt.nde18074@vtc.edu.vn>
2021-02-06 21:37:52 +0700

    Cart Screen

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,12 +1,146 @@
-import React from 'react'
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  makeStyles,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import PaymentMethodScreen from "../screens/PaymentMethodScreen";
+import PlaceOrderScreen from "../screens/PlaceOrderScreen";
+import ShippingAddressScreen from "../screens/ShippingAddressScreen";
 
 export default function CheckoutSteps(props) {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
   return (
-    <div className='row checkout-steps'>
-      <div className={props.step1 ? 'active' : ''}>Sign-In</div>
-      <div className={props.step2 ? 'active' : ''}>Shipping</div>
-      <div className={props.step3 ? 'active' : ''}>Payment</div>
-      <div className={props.step4 ? 'active' : ''}>Place Order</div>
-    </div>
-  )
+    // <div className='row checkout-steps'>
+    //   <div className={props.step1 ? 'active' : ''}>Sign-In</div>
+    //   <div className={props.step2 ? 'active' : ''}>Shipping</div>
+    //   <div className={props.step3 ? 'active' : ''}>Payment</div>
+    //   <div className={props.step4 ? 'active' : ''}>Place Order</div>
+    // </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Company name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
+                </Typography>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <div className={classes.buttons}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} className={classes.button}>
+                      Back
+                    </Button>
+                  )}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                  </Button>
+                </div>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        </Paper>
+      </main>
+    </React.Fragment>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const steps = ["Shipping address", "Payment details", "Review your order"];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <ShippingAddressScreen />;
+    case 1:
+      return <PaymentMethodScreen />;
+    case 2:
+      return <PlaceOrderScreen />;
+    default:
+      throw new Error("Unknown step");
+  }
 }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,7 +12,11 @@ import {
   TableRow,
   TableCell,
   makeStyles,
+  Button,
+  Container,
+  Grid,
 } from "@material-ui/core";
+import { Delete as DeleteIcon } from "@material-ui/icons";
 function ccyFormat(num) {
   return `${num.toFixed(2)}`;
 }
@@ -121,41 +125,53 @@ export default function CartScreen(props) {
     //   </div>
     // </div>
 
-    // <Container maxWidth="md">
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" colSpan={3}>
-              Details
-            </TableCell>
-            <TableCell align="right">Price ($)</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Product Name</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Sum</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cartItems.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.name}</TableCell>
-              <TableCell align="right">{item.qty}</TableCell>
-              <TableCell align="right">{item.price}</TableCell>
-              <TableCell align="right">{ccyFormat(item.price)}</TableCell>
-            </TableRow>
-          ))}
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={10}>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="spanning table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" colSpan={3}>
+                    Details
+                  </TableCell>
+                  <TableCell align="right">Price ($)</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Product Name</TableCell>
+                  <TableCell align="right">Quantity</TableCell>
+                  <TableCell align="right">Price</TableCell>
+                  <TableCell align="right">Sum</TableCell>
+                  <TableCell align="right"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {cartItems.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell align="right">{item.qty}</TableCell>
+                    <TableCell align="right">{item.price}</TableCell>
+                    <TableCell align="right">{ccyFormat(item.price)}</TableCell>
+                    <TableCell align="right">
+                      <Button
+                        onClick={() => removeFromCartHandler(item.product)}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
 
-          <TableRow>
-            <TableCell rowSpan={3} />
-            <TableCell colSpan={2}>Subtotal</TableCell>
-            <TableCell align="right">
-              {ccyFormat(cartItems.reduce((a, c) => a + c.price * c.qty, 0))}
-            </TableCell>
-          </TableRow>
-          {/* <TableRow>
+                <TableRow>
+                  <TableCell rowSpan={3} />
+                  <TableCell colSpan={2}>Subtotal</TableCell>
+                  <TableCell align="right">
+                    {ccyFormat(
+                      cartItems.reduce((a, c) => a + c.price * c.qty, 0)
+                    )}
+                  </TableCell>
+                </TableRow>
+                {/* <TableRow>
             <TableCell>Tax</TableCell>
             <TableCell align="right">{`${(TAX_RATE * 100).toFixed(
               0
@@ -166,10 +182,22 @@ export default function CartScreen(props) {
             <TableCell colSpan={2}>Total</TableCell>
             <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
           </TableRow> */}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    // </Container>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={checkoutHandler}
+            disabled={cartItems.length === 0}
+          >
+            Proceed to Checkout
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 const useStyles = makeStyles({
